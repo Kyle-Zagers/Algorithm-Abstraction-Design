530b5b16a689848632c324522f3ea86798dd8bee
Kyle-Zagers <kylezagers@gmail.com>
2024-11-12 17:59:42 -0500

    finished program 4

---
+++ Milestone 2/program4.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+/* Solution to program 4
+* @param n the number of paintings
+* @param W the maximum width of the platform
+* @param heights the heights of the paintings
+* @param widths the widths of the paintings
+* @return a tuple containing the number of platforms used, the optimal total height, and the number of paintings on each platform
+*/
+using namespace std;
+
+std::tuple<int, int, std::vector<int>> program4(int n, int W, std::vector<int> heights, std::vector<int> widths){
+    vector<int> opt(n+1, INT_MAX);
+    vector<int> platformStart(n+1, 0);
+    opt[0] = 0;
+
+    for (int i = 1; i <= n; i++){
+        int width = 0;
+        int maxHeight = 0;
+        for(int j = i; j > 0; j--){
+            width += widths[j-1];
+            if(width > W){
+                break;
+            }
+            maxHeight = max(maxHeight, heights[j-1]);
+            if(opt[j-1] + maxHeight < opt[i]){
+                opt[i] = opt[j-1] + maxHeight;
+                platformStart[i] = j;
+            }
+        }
+    }
+
+    int numPlatforms = 0;
+    vector<int> platformSizes;
+
+    for(int i = n; i > 0; i = platformStart[i]-1){
+        numPlatforms++;
+        platformSizes.push_back(i - platformStart[i] + 1);
+    }
+    reverse(platformSizes.begin(), platformSizes.end());
+
+    return std::make_tuple(numPlatforms, opt[n], platformSizes);
+
+//    return std::make_tuple(0, 0, std::vector<int>()); // replace with your own result.
+    // return std::make_tuple(n, 0, heights);
+}
+int main(){
+    int n, W;
+    std::cin >> n >> W;
+    std::vector<int> heights(n);
+    std::vector<int> widths(n);
+    for(int i = 0; i < n; i++){
+        std::cin >> heights[i];
+    }
+    for(int i = 0; i < n; i++){
+        std::cin >> widths[i];
+    }
+    auto result = program4(n, W, heights, widths);
+
+    std::cout << std::get<0>(result) << std::endl;
+    std::cout << std::get<1>(result) << std::endl;
+    for(int i = 0; i < std::get<0>(result); i++){
+        std::cout << std::get<2>(result)[i] << std::endl;
+    }
+    return 0; 
+}
